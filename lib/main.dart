a0bff99cbb941a3b0dbb2aad3abac35e3dc8a4dd
DitRadit <adiradityagt@gmail.com>
2025-02-14 17:37:19 +0700

    update perbaikan authentication

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase/pages/signUpPage.dart';
 import 'package:firebase/pages/todosPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -13,35 +12,37 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await checkSignInStatus();
-  runApp(MyApp());
-}
 
-Future<void> checkSignInStatus() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? userId = prefs.getString('userId');
-  try {
-    if (userId != null) {
-      Get.offNamed('/todos');
-    } else {
-      Get.offNamed('/sign-in');
-    }
-  } catch (e) {
-    Get.offNamed('/sign-in');
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: [
-        GetPage(name: '/sign-in', page: () => SignInPage()),
-        GetPage(name: 'sign-up', page: () => SignUpPage()),
-        GetPage(name: '/todos', page: () => TodosPage()),
-      ],
+    return FutureBuilder(
+      future: checkSignInStatus(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+              child: CircularProgressIndicator()); // Loading state
+        }
+
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: snapshot.data == true ? '/todos' : '/sign-in',
+          getPages: [
+            GetPage(name: '/sign-in', page: () => SignInPage()),
+            GetPage(name: '/sign-up', page: () => SignUpPage()),
+            GetPage(name: '/todos', page: () => TodosPage()),
+          ],
+        );
+      },
     );
+  }
+
+  Future<bool> checkSignInStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userId = prefs.getString('userId');
+    return userId != null;
   }
 }
